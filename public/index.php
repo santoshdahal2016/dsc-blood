5b80d35c12d2dbaab137029bf075deb6dd06f6b3
santosh <webtutor2016@gmail.com>
2017-11-24 07:41:31 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
-
+echo "youtube";
 define('LARAVEL_START', microtime(true));
 
 /*
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
