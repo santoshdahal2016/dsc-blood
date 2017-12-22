f1aa1eff002efa8130b4353b68e0d3df86ffbc93
santoshdahal2016 <webtutor2016@gmail.com>
2017-12-22 20:42:56 +0545

    database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'bloodincloudsql.mysql.database.azure.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'bloodincloud'),
+            'username' => env('DB_USERNAME', 'santosh@bloodincloudsql'),
+            'password' => env('DB_PASSWORD', 'Bl00dincl0ud'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
