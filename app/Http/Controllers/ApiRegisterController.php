176b1e552b9bd40c6b77f992bb268b805b6504d0
santoshdahal2016 <webtutor2016@gmail.com>
2020-01-28 22:44:40 +0545

    registration android api

--- app/Http/Controllers/ApiRegisterController.php
+++ app/Http/Controllers/ApiRegisterController.php
@@ -110,8 +110,8 @@ class ApiRegisterController extends controller
         }
         $request1 = new \Illuminate\Http\Request();
 
-        $request1->add(['username' => $request->phone]);
-        $request1->add(['password' => $request->password]);
+        $request1->request->add(['username' => $request->phone]);
+        $request1->request->add(['password' => $request->password]);
 
         error_log( print_r($request1->all(), TRUE));
 
